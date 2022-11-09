735fd4f10b68166f9b573bbd3e6a5c35a9331684
ignatio <bigbrisco@gmail.com>
2022-11-09 10:44:20 -0500

    Restore '11/9/2022, 10:20:38 AM 1.0.3'

--- main.ts
+++ main.ts
@@ -26,7 +26,7 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`flagRed`, function (sprite, l
         invulnerable = 1
         music.powerUp.play()
         timer.after(5000, function () {
-            if (currentLevel < 6) {
+            if (currentLevel < 4) {
                 currentLevel += 1
                 nextLevel()
             } else {
